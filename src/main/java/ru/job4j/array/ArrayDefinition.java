338eaa7d72b8215e7d6e0843293805bbcfb1bf6f
aslanmagamaev <aslanmagamaev@mail.ru>
2022-01-09 04:11:42 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Aslan Magamaev";
+        names[1] = "Ivan Tarskii";
+        names[2] = "Mikhail Cherednichenko";
+        names[3] = "Artem Akchurin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
      }
 }
