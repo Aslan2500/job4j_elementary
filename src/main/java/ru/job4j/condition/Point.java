9198bd842d4b9decba2b5c178a59f69e1e3212fb
aslanmagamaev <aslanmagamaev@mail.ru>
2022-01-12 18:03:30 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,7 +4,6 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
 public class Point {
-
     private int x;
     private int y;
 
@@ -24,3 +23,4 @@ public class Point {
         System.out.println(dist);
     }
 }
+
